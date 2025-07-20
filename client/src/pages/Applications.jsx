4d90dedd9ef8be2894a6a9c07455471d6a67abab
Pradeep Pal <palpradeep0712@gmail.com>
2025-07-20 10:15:20 +0530

    change job application status

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import { assets, jobsApplied } from '../assets/assets'
 import moment from 'moment'
@@ -16,7 +16,7 @@ const Applications = () => {
   const [isEdit, setIsEdit] = useState(false)
   const [resume, setResume] = useState(null)
 
-  const {backendUrl, userData, userApplications, fetchUserData} = useContext(AppContext)
+  const {backendUrl, userData, userApplications, fetchUserData, fetchUserApplications} = useContext(AppContext)
 
   const updateResume = async () => {
     try {
@@ -47,6 +47,12 @@ const Applications = () => {
 
   }
 
+  useEffect(() => {
+    if(user){
+      fetchUserApplications()
+    }
+  },[user])
+
   return (
     <>
       <Navbar/>
@@ -54,7 +60,7 @@ const Applications = () => {
         <h2 className='text-xl font-semibold'>Your Resume</h2>
         <div className='flex gap-2 mb-6 mt-3'>
           {
-            isEdit || userData && userData.resume === ''
+            isEdit || userData && userData?.resume === ''
             ? <>
             <label className='flex items-center' htmlFor="resumeUpload">
               <p className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2'>{resume ? resume.name : 'Select Resume'}</p>
@@ -64,7 +70,7 @@ const Applications = () => {
             <button onClick={updateResume} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2'>Save</button>
             </>
             : <div className='flex gap-2'>
-              <a classNae='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg' href="">
+              <a target='_blank' href={userData?.resume} className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg'>
                 Resume
               </a>
               <button onClick={() => setIsEdit(true)} className='text-gray-500 border border-e-gray-300 rounded-lg px-4 py-2'>
--- client/src/pages/ManageJob.jsx
+++ client/src/pages/ManageJob.jsx
@@ -5,12 +5,13 @@ import { useNavigate } from 'react-router-dom'
 import AppContext from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import Loading from '../components/Loading'
 
 const ManageJob = () => {
 
   const navigate = useNavigate()
 
-  const [jobs, setJobs] = useState([])
+  const [jobs, setJobs] = useState(false)
 
   const { backendUrl, companyToken } = useContext(AppContext)
 
@@ -63,7 +64,11 @@ const ManageJob = () => {
     }
   },[companyToken])
 
-  return (
+  return jobs ? jobs.length === 0 ? (
+  <div className='flex items-center justify-center h-[70vh]'>
+    <p className='text-xl sm:text-2xl'>No Jobs Available or Posted</p>
+  </div>
+  ) : (
     <div className='container p-4 max-w-5xl'>
       <div className='overflow-x-auto'>
         <table className='min-w-full bg-white border border-gray-200 max-sm:text-sm'>
@@ -99,7 +104,7 @@ const ManageJob = () => {
         <button onClick={() => navigate('/dashboard/add-job')} className='bg-black text-white py-2 px-4 rounded'>Add New Job</button>
       </div>
     </div>
-  )
+  ) : <Loading/>
 }
 
 export default ManageJob
